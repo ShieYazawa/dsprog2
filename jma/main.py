b4fd6f3a9f165ec51d54f562cdc333d4f7ccfa08
ShieYazawa <s2422061@stu.musashino-u.ac.jp>
2024-11-27 10:21:50 +0900

    weather.jsonファイルとmain.pyに追記

--- jma/main.py
+++ jma/main.py
@@ -1,7 +1,16 @@
 import flet as ft
-
+import requests
 
 def main(page: ft.Page):
+
+    # 地域データの取得
+    URL = "http://www.jma.go.jp/bosai/common/const/area.json"
+    data_json = requests.get(URL).json()
+    
+    # 天気データの取得（例：東京の場合）
+    weather_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
+    weather_data = requests.get(weather_URL).json()
+
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
 
 
